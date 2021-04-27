7513884e6df8e97d2cc7b407c8b31dc937683501
Pablo Carvelli Vargas <pablocarvellivargas@gmail.com>
2021-04-27 16:34:38 +0200

    3da

--- src/pack01/Menu.java
+++ src/pack01/Menu.java
@@ -185,8 +185,7 @@ public class Menu {
         System.out.println("Si: introduzca un 1, No: introduzca un 0:");
         int respuesta = entradaUsuario.nextInt();
 
-        if(respuesta != 1){
-
+        while(respuesta != 1){
             System.out.println("Que doato desea corregir?");
             System.out.println("1. Codigo.");
             System.out.println("2. Direccion.");
@@ -321,7 +320,31 @@ public class Menu {
                 default:
                     System.out.println("Opcion incorrecta.");
             }
+
+            System.out.println("Verifique que los datos esten correctos.");
+            System.out.println("Codigo: " + p.getCodigo());
+            System.out.println("Direccion: " + p.getDireccion());
+            System.out.println("Numero: " + p.getNumero());
+            System.out.println("Piso: " + p.getPiso());
+            System.out.println("Propietario: " + p.getPropietario());
+            System.out.println("Fecfa inicio Cedido: " + p.getInicioCedido().toString());
+            System.out.println("Fecha fin Cedido: " + p.getFinalCedido().toString());
+            System.out.println("Metros cuadrados: " + p.getMetrosCuadrados());
+            System.out.println("Numero de camas: " + p.getNumeroCamas());
+            System.out.println("Baño: " + p.getBano());
+            System.out.println("Comedor:" + p.getComedor());
+            System.out.println("Servicio de comidas: " + p.getServicioComida());
+            System.out.println("Internet: " + p.getInternet());
+            System.out.println("Exterior: " + p.getExterior());
+            System.out.println("Planta baja: " + p.getPlantaBaja());
+
+            System.out.println("\n\nSon correctos estos datos?");
+            System.out.println("Si: introduzca un 1, No: introduzca un 0:");
+            respuesta = entradaUsuario.nextInt();
         }
+
+        // if(respuesta != 1){ }
+
         entradaUsuario.close();
         return p;
     }
