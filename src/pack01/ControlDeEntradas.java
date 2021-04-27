8735148243edd4f0925b87dd7b4b4f65628638fb
Pablo Carvelli Vargas <pablocarvellivargas@gmail.com>
2021-04-27 16:57:09 +0200

    5ta

--- src/pack01/ControlDeEntradas.java
+++ src/pack01/ControlDeEntradas.java
@@ -4,12 +4,30 @@ import java.util.Scanner;
 
 public class ControlDeEntradas {
 
-    public static int pedirNumeroEnteroPositivo(){
-        int n = -1;
+    public static int entradaoEnteroPositivo(){
+        int n;
         Scanner entradaUsuario = new Scanner(System.in);
-        while(n < 0){
+        do{
             n = entradaUsuario.nextInt();
-        }
+        }while(n < 0);
+        return n;
+    }
 
+    public static float entradaFlotantePositivo(){
+        float n;
+        Scanner entradaUsuario = new Scanner(System.in);
+        do{
+            n = entradaUsuario.nextFloat();
+        }while(n < 0);
+        return n;
+    }
+
+    public static String entradaString(){
+        String s;
+        Scanner entradaUsuario = new Scanner(System.in);
+        do{
+            s = entradaUsuario.nextLine();
+        }while(s.length() <= 0);
+        return s;
     }
 }
